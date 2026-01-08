d4c46259c2d6823a4372472985adeabdc0d7461d
Sumaiya-Afrin-Eva <sumaiya.afrin.evaa@gmail.com>
2026-01-08 23:39:04 +0600

    Updated Project Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,26 +30,21 @@ android {
 }
 
 dependencies {
-    // Firebase
     implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
     implementation("com.google.firebase:firebase-database")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-storage")
 
-    // AndroidX
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 
-    // RecyclerView
     implementation("androidx.recyclerview:recyclerview:1.4.0")
 
-    // Networking
     implementation("com.squareup.okhttp3:okhttp:5.3.2")
     implementation("com.google.code.gson:gson:2.13.2")
 
-    // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
