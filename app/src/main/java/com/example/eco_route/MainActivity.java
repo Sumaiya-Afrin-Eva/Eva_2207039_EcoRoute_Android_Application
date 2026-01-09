474185e03464ec9f2f527b71c518d8b2621f3148
Sumaiya-Afrin-Eva <sumaiya.afrin.evaa@gmail.com>
2026-01-09 22:38:14 +0600

    Added MainActivity

--- app/src/main/java/com/example/eco_route/MainActivity.java
+++ app/src/main/java/com/example/eco_route/MainActivity.java
@@ -1,24 +1,38 @@
 package com.example.eco_route;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin, btnSignupPartner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_home); // change if needed
+
+        btnLogin = findViewById(R.id.loginBtn);
+        btnSignupPartner = findViewById(R.id.routePartnerBtn);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UserLoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSignupPartner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AdminLoginActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
