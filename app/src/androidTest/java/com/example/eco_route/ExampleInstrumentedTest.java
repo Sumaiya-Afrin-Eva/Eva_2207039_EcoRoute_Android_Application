1d0f16582b16d112ea3aa0afb8ed32bccff1af0c
Sumaiya-Afrin-Eva <sumaiya.afrin.evaa@gmail.com>
2026-01-10 15:13:59 +0600

    Updated AndroidManifest.xml

--- app/src/androidTest/java/com/example/eco_route/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/eco_route/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.eco_route", appContext.getPackageName());
+        assertEquals("com.example.ecorouteandroid", appContext.getPackageName());
     }
 }
