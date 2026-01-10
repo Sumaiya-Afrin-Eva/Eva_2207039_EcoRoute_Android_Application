fe19f0e14d7b1d437961cd592c6312fa65463b88
Sumaiya-Afrin-Eva <sumaiya.afrin.evaa@gmail.com>
2026-01-10 14:59:55 +0600

    Updated CodeVerificationActivity

---
+++ app/src/main/java/com/example/eco_route/CodeVerificationActivity.java
@@ -0,0 +1,30 @@
+package com.example.eco_route;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.eco_route.UserActivity;
+
+public class CodeVerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_code_verification);
+
+        Button verifyButton = findViewById(R.id.verifyCode);
+
+        verifyButton.setOnClickListener(v -> {
+            Intent intent = new Intent(
+                    CodeVerificationActivity.this,
+                    UserActivity.class
+            );
+            startActivity(intent);
+        });
+    }
+}
---
+++ app/src/main/java/com/example/eco_route/UserLoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.eco_route;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ecorouteandroid.R;
+
+public class UserLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_user_login);
+
+        Button btnContinueWithEmail = findViewById(R.id.emailButton);
+
+        btnContinueWithEmail.setOnClickListener(v -> {
+            Intent intent = new Intent(UserLoginActivity.this, EmailLoginActivity.class);
+            startActivity(intent);
+        });
+    }
+}
